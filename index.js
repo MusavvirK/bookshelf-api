5250527e8d89cfca2fe3f2a473d1ba3a47ca6f16
Abde Musavvir Khan <musavvir@debian-musavvir>
2020-12-28 08:49:35 +0600

    setting up to push to heroku

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const mongoose = require("mongoose");
 // const cors = require('cors');  // we don't need it anymore, because we use proxy server instead
 
 // DB Setup (connect mongoose and instance of mongodb)
-mongoose.connect("mongodb://localhost:blog/blog");
+mongoose.connect("process.env.MONGODB_URI", {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
 
 // App Setup (morgan and body-parser are middleware in Express)
 app.use(morgan("combined")); // middleware for logging
